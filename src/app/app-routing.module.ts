f797cd6f1319a6b9af00326de5f2a2671c7ea6d1
J Rivas <jrivasgonzalez@gmail.com>
2018-09-26 11:02:35 +0100

    Rutas implementadas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ItemComponent } from './pages/item/item.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/', pathMatch: 'full' },
+    { path: 'home', component: PortfolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'item', component: ItemComponent },
+
+    // otherwise redirect to 404
+    // { path: '**', redirectTo: '/' + AppConfig.routes.error404 }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes, {
+            scrollPositionRestoration: 'enabled',
+            anchorScrolling: 'enabled',
+            useHash: true
+        })
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule { }
