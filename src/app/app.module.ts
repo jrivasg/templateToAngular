c6e4c523b981c2b82a7d1528ef06ffe1b2248e8d
J Rivas <jrivasgonzalez@gmail.com>
2018-09-26 11:54:58 +0100

    Primeras animaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 // Páginas
 import { AppComponent } from './app.component';
@@ -23,7 +24,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
